80fd4a41baacff38afdb98caba9e6d9f921f5f57
RengaRamanujam <107357468+RengaRamanujam@users.noreply.github.com>
2024-05-08 14:10:22 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// const Header = () => {
-//   return (
-//     <div className='flex rounded-xl mx-1 mt-2 bg-blue-200 p-4 text-2xl font-sans'>
-//       <div className="flex justify-between items-center">
-//         <div className="flex space-x-4">
-//           <Link to={'/'} className='"text-black mx-4 no-underline hover:text-green-300"'>Home</Link>
-//           <Link to='/about' className={"text-black mx-4 no-underline hover:text-green-300"}>About Us</Link>
-//           <Link to={'/cart'} className="text-black mx-4 no-underline hover:text-green-300">Cart</Link>
-//         </div>
-//         <div className="flex space-x-4">
-//           <Link to='/login' className="text-black mx-4 no-underline hover:text-green-300">Login</Link>
-//           <Link to='/signup' className="text-black mx-4 no-underline hover:text-green-300">Signup</Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Header;
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 
